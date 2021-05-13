4a98f049046c132d3d31018cc030338daf812f48
Ubuntu <ubuntu@ip-172-31-46-9.us-west-2.compute.internal>
2021-05-13 17:46:19 +0000

    temp commit

--- test_lu.py
+++ test_lu.py
@@ -1,35 +1,34 @@
-import argparse
-
-from nums.core.application_manager import instance
-from nums.core.array.blockarray import BlockArray
-from nums.core.array.application import ArrayApplication
-from nums.core.systems.systems import System
 import time
+import ray
+from nums.core.application_manager import instance
+from nums.core.array.application import ArrayApplication
 
-import numpy as np
-from scipy.linalg import lu as scipy_lu
-
-
-def main(n, b):
+def main(n, b, c):
+  ray.init(**{
+      "num_cpus": c
+  }) 
   n = int(n)
   b = int(b)
   app: ArrayApplication = instance()
   times = []
-  for _ in range(5):
+  for _ in range(1):
     A = app.random.random((n, n//1024), (b, b))
-    t_start = time.time()
     # Put experiment here
 
     X = (A @ A.T).reshape(block_shape=(b, b))
-    _ = app.inv(X)
+    t_start = time.time()
+    _ = app.lu_inv(X)
 
     # Ecperiment done
     times.append(time.time() - t_start)
-  print(str([n, b, times]) + ",")
+  print(str([n, b, c,  times]) + ",")
+  ray.shutdown()
 
 if __name__ == "__main__":
-  for k in range(10):
-    for l in range(10):
-      b = 1024 * 2 ** l
-      main(b * 2 ** k, b)
+
+  # b = 1024 * 2 ** 4
+  # Strong scaling
+  n = 1024 * 2 ** 8
+  for c in [1, 2, 4, 8, 16, 32, 64]:
+    main(n, n // 8, c)
   print("done!")
